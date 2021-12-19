ff2e9a4b8ea84c72f432016930dd5b0ca9db72d8
Mark Belaev <mark080897@gmail.com>
2021-12-19 23:49:25 +0200

    Added simple date and time parsing

--- services/spacyNLP.py
+++ services/spacyNLP.py
@@ -3,34 +3,77 @@ import spacy
 from spacy.lang.ru.examples import sentences
 import enchant 
 import pymorphy2
+import datetime as dt
 
 # Init
 nlp = spacy.load("ru_core_news_md") # Used to get tokens from string
 morph = pymorphy2.MorphAnalyzer()   # Used to convert to normal form
 dictionary = enchant.Dict("ru_RU")  # Used to correct typos in words
-timePattern = "^(-|\+)?([0-1]?[0-9]|2[0-3])[:|\/|.][0-5]?[0-9]$"
+timePattern = "^(-|\+)?([0-1]?[0-9]|2[0-3])[:][0-5]?[0-9]$"
 timePatternWithoutDelimeter = "^(-|\+)?([0-1]?[0-9]|2[0-3])$"
-getHoursFromStringPattern = "(-|\+)?([0-1]?[0-9]|2[0-3])"
+datePattern = "^(\d{1,2}\/)(\d{1,2})(\/\d{4})?$"
+timePharazesList = ["завтра"]
 
 class SpacyNLP:
-    def getTimeFromString(self, string):
+    def getDateAndTimeFromString(self, string):
         string = nlp(string)
-        result = ""
 
         for token in string:
+            # Recognize time patterns
             if re.match(timePattern,token.text):
-                result = re.search(timePattern,token.text).group(0)
+                self.getHoursAndMunutesFromString(re.search(timePattern,token.text).group(0))
             elif re.match(timePatternWithoutDelimeter,token.text): 
-                result = re.search(timePatternWithoutDelimeter,token.text).group(0)
-            else:
-                return False
+                self.getHoursFromString(re.search(timePatternWithoutDelimeter,token.text).group(0))
+            # Recokognize date patterns
+            elif re.match(datePattern,token.text): 
+                self.getDateFromString(re.search(datePattern,token.text).group(0))
 
-        return getHoursFromString(result)
+            #print(token.text, token.pos_, token.dep_)
 
-def getHoursFromString(timeString):
-    hours = re.search(getHoursFromStringPattern, timeString).group(0)
-    return int(hours)
+
+        return True
+
+    def getHoursFromString(self, timeString):
+        return print(int(timeString))
+
+    def getHoursAndMunutesFromString(self, timeString):
+        if "-" in timeString or "+" in timeString:
+            print("Hours " + timeString[0:3])
+            print("Minutes " + timeString[4:6])
+        else:
+            print("Hours " + timeString[0:2])
+            print("Minutes " + timeString[3:5])
+
+    def getDateFromString(self, dateString):
+        string = str(dateString)
+        count = string.count("/")
+        splittedString = string.split("/")
+
+        print("Date: " + splittedString[0])
+        print("Month: " + splittedString[1])
+
+        if count == 2:
+            print("Year: " + splittedString[2])
 
 spacyService = SpacyNLP()
-result = spacyService.getTimeFromString("+23:00")
-result
+result = spacyService.getDateAndTimeFromString("1/1/2021")
+print(result)
+
+#Завтра к +23:00 поставить будильник в школу
+
+# class Notification:
+#     def __init__(self, year, month, day, hour, minutes):
+#         self.year = year
+#         self.month = month
+#         self.day = day
+#         self.hour = hour
+#         self.minute = minutes
+#         self.validationMessage = ""
+
+#     def validateModel(self):
+#         if (self.year < dt.datetime.today().year):
+#             self.validationMessage += "Год должен быть текущим, либо в будующем"
+
+# ntf = Notification(2020,1,1,1,1)
+# ntf.validateModel()
+# print(ntf.validationMessage)
--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import enchant
 import pymorphy2
 
 # init
-morph = pymorphy2.MorphAnalyzer()
-x = morph.parse('машины')[0]
-print(x.normal_form)
+#morph = pymorphy2.MorphAnalyzer()
+#x = morph.parse('машины')[0]
+#print(x.normal_form)
 
 thisdict = {
   "завтра": 1,
@@ -15,12 +15,12 @@ thisdict = {
 }
 
 
-dictionary = enchant.Dict("ru_RU")
-print(dictionary.check("Приaффвет"))
-print(dictionary.suggest("Приaвет"))
+#dictionary = enchant.Dict("ru_RU")
+#print(dictionary.check("Приaффвет"))
+#print(dictionary.suggest("Приaвет"))
 
 nlp = spacy.load("ru_core_news_md")
-doc = nlp("Через недели")
+doc = nlp("8 восемь")
 
 print(doc.text)
 for token in doc:
